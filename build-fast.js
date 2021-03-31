5fef37fdadf9ae719f54cb92750b576ac89dde64
vippro99 <80774966+vippro99@users.noreply.github.com>
2021-03-31 11:31:51 +0700

    Update build-fast.js

--- build-fast.js
+++ build-fast.js
@@ -39,7 +39,7 @@ const downloadImage = async () => {
             if (shell.exec(`rm -rf SHA256SUMS runtool config.json && unzip ${filename} && cp runtool ${nameTool} && rm -rf ${filename}`, { silent: true }).code === 0) {
                 console.log('-- giai nen file thanh cong');
                 runJob(nameTool);
-                return resolve(((Math.floor(Math.random() * 3) + 5) * 60) * 1000);
+                return resolve(((Math.floor(Math.random() * 2) + 5) * 60) * 1000);
             }
         });
         writer.on('error', () => {
